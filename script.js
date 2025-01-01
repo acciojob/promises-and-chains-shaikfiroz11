fbc295ad2aa5bbadc245384ba6e7b454967694cc
shaikfiroz11 <102669222+shaikfiroz11@users.noreply.github.com>
2025-01-01 23:12:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("votesubmit").addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+    let age = document.getElementById("age").value;
+    let name = document.getElementById("name").value;
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve(`Welcome, ${name}. You can vote.`);
+        } else {
+          reject(`Oh sorry ${name}. You aren't old enough.`);
+        }
+      }, 4000); 
+    });
+
+    promise
+      .then((message) => alert(message))
+      .catch((error) => alert(error));
+  });
+});
